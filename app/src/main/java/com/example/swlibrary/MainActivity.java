3c2c3edc15c02cabef97a8f5eb7542cac8b5392b
TrenkyB <timotej442@gmail.com>
2022-08-16 13:13:22 +0200

    Kulo

--- app/src/main/java/com/example/swlibrary/MainActivity.java
+++ app/src/main/java/com/example/swlibrary/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.swlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+Button help;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+       help = findViewById(R.id.Help);
+       help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Help.class));
+            }
+        } );
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
