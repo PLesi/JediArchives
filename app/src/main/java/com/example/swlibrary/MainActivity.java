efe1f4d10efeb638989f4e0ca2f9ca7068b65be7
TrenkyB <timotej442@gmail.com>
2022-08-16 16:05:07 +0200

    Kulo

--- app/src/main/java/com/example/swlibrary/MainActivity.java
+++ app/src/main/java/com/example/swlibrary/MainActivity.java
@@ -13,18 +13,19 @@ Button help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
        help = findViewById(R.id.Help);
        help.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,Help.class));
             }
-        } );
+        });
 
 
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
     }
 }
