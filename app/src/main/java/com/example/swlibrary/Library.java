835fbc20ad743e581f1bafe69805bf6989a2b0c9
TrenkyB <timotej442@gmail.com>
2022-08-19 11:31:16 +0200

    Kulo

--- app/src/main/java/com/example/swlibrary/Library.java
+++ app/src/main/java/com/example/swlibrary/Library.java
@@ -1,11 +1,13 @@
 package com.example.swlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
-
-
 import com.squareup.picasso.Picasso;
 
 
@@ -16,8 +18,19 @@ public class Library extends AppCompatActivity {
         setContentView(R.layout.activity_library);
 
         ImageView imageView = findViewById(R.id.Cross);
+
+        Button personBtn = findViewById(R.id.personBtn);
+        personBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Library.this, Personalities.class));
+            }
+        });
+
         Picasso.with(this)
                 .load("https://i.imgflip.com/2gro1p.jpg")
                 .into(imageView);
+
+
     }
 }
