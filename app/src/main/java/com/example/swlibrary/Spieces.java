1563ac5f79b663798a9166d1da1b38aa0e82452c
PLesi <mplesko260@gmail.com>
2022-08-19 11:54:37 +0200

    spieces

--- app/src/main/java/com/example/swlibrary/Spieces.java
+++ app/src/main/java/com/example/swlibrary/Spieces.java
@@ -2,13 +2,40 @@ package com.example.swlibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Spieces extends AppCompatActivity {
+
+    Button planetsBtn = findViewById(R.id.planetsBtn);
+    Button technoBtn = findViewById(R.id.technoBtn);
+    Button personBtn = findViewById(R.id.personBtn);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spieces);
+
+        planetsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Spieces.this, Library.class));
+            }
+        });
+        technoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Spieces.this, Technology.class));
+            }
+        });
+        personBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Spieces.this,Personalities.class));
+            }
+        });
+
     }
 }
