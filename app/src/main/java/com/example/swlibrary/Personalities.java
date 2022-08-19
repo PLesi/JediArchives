89f1a0b9abe61aed8b826063c696896f77023d78
PLesi <mplesko260@gmail.com>
2022-08-19 10:28:14 +0200

    image load

---
+++ app/src/main/java/com/example/swlibrary/Personalities.java
@@ -0,0 +1,14 @@
+package com.example.swlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Personalities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personalities);
+    }
+}
---
+++ app/src/main/java/com/example/swlibrary/Spieces.java
@@ -0,0 +1,14 @@
+package com.example.swlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Spieces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spieces);
+    }
+}
