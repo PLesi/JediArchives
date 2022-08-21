cc588bf63fa20fea1e11607e19e6bb8df7a0e86b
PLesi <mplesko260@gmail.com>
2022-08-21 19:46:21 +0200

    nová aktivita

--- app/src/main/java/com/example/swlibrary/Informations.java
+++ app/src/main/java/com/example/swlibrary/Informations.java
@@ -3,12 +3,25 @@ package com.example.swlibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Informations extends AppCompatActivity {
+
+    ImageButton imageButton = findViewById(R.id.buttonBack);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            finish();
+            }
+        });
     }
+
+
 }
